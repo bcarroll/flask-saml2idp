e958de5cf647821d0bb6c8784b1f64c3a1c91b59
Yoeri Otten <yoeori@live.nl>
2019-12-14 20:37:13 +0100

    Update configuration

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-import djangosaml2idp
-from djangosaml2idp import __version__
+import flasksaml2idp  # noqa
+from flasksaml2idp import __version__  # noqa
 
 # -- General configuration ------------------------------------------------
 
@@ -106,7 +106,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'djangosaml2idpdoc'
+htmlhelp_basename = 'flasksaml2idpdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -133,7 +133,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'djangosaml2idp.tex', 'djangosaml2idp Documentation',
+    (master_doc, 'flasksaml2idp.tex', 'flasksaml2idp Documentation',
      'Mathieu Hinderyckx', 'manual'),
 ]
 
@@ -143,7 +143,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'djangosaml2idp', 'djangosaml2idp Documentation',
+    (master_doc, 'flasksaml2idp', 'flasksaml2idp Documentation',
      [author], 1)
 ]
 
@@ -154,7 +154,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'djangosaml2idp', 'djangosaml2idp Documentation',
-     author, 'djangosaml2idp', 'SAML2 Identity Provider in Django.',
+    (master_doc, 'flasksaml2idp', 'flasksaml2idp Documentation',
+     author, 'flasksaml2idp', 'SAML2 Identity Provider in Flask.',
      'Miscellaneous'),
 ]
