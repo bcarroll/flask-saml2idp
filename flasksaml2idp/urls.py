fac53a6825d9bb797fb0a58ff703e58f2fdd2132
Yoeri Otten <yoeori@live.nl>
2019-12-14 22:47:25 +0100

    Fix class views

--- flasksaml2idp/urls.py
+++ flasksaml2idp/urls.py
@@ -6,8 +6,8 @@ blueprint = Blueprint('flasksaml2idp', __name__, template_folder='templates')
 
 blueprint.add_url_rule('/sso/post', 'saml_login_post', views.sso_entry, methods=['GET', 'POST'])
 blueprint.add_url_rule('/sso/redirect', 'saml_login_redirect', views.sso_entry)
-blueprint.add_url_rule('/sso/init', 'saml_idp_init', views.SSOInitView)
-blueprint.add_url_rule('/login/process/', 'saml_login_process', views.LoginProcessView)
-blueprint.add_url_rule('/login/process_multi_factor/', 'saml_multi_factor', views.ProcessMultiFactorView,
-                       methods=['GET'])
+blueprint.add_url_rule('/sso/init', 'saml_idp_init', views.SSOInitView.as_view('saml_idp_init'))
+blueprint.add_url_rule('/login/process/', 'saml_login_process', views.LoginProcessView.as_view('saml_login_process'))
+blueprint.add_url_rule('/login/process_multi_factor/', 'saml_multi_factor',
+                       views.ProcessMultiFactorView.as_view('saml_multi_factor'), methods=['GET'])
 blueprint.add_url_rule('/metadata/', 'saml2_idp_metadata', views.metadata)
